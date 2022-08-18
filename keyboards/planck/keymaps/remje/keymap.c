36f0219e8bcc8769d002289297fbe903656abfe4
remje <arenwpro@gmail.com>
2022-08-18 14:02:39 +0200

    first functional version

--- keyboards/planck/keymaps/remje/keymap.c
+++ keyboards/planck/keymaps/remje/keymap.c
@@ -27,72 +27,65 @@ enum planck_layers {
 };
 
 enum planck_keycodes {
-  AZERTY,
-  BACKLIT,
-  EXT_PLV
+  AZERTY = SAFE_RANGE,
+  BACKLIT
 };
 
-//tap dance declarations
-enum {
-	TD_YANK,
-	TD_PASTE
-};
-
-#define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
+#define LOWER MO(_LOWER)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Azerty
  * ,-----------------------------------------------------------------------------------.
- * | Del  |   A  |   Z  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp | V
+ * | Del  |   A  |   Z  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   Q  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   M  |  '   | V
+ * | Tab  |   Q  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   M  |  '   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   >  |   W  |   X  |   C  |   V  |   B  |   N  |   ,  |   .  |   /  |Enter | V
+ * | Shift|   >  |   W  |   X  |   C  |   V  |   B  |   N  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  | Ctrl | Alt  | GUI  |Lower |    Space    |Raise |   é  |   à  |   è  |AltGr | V
+ * | Esc  | Ctrl | Alt  | GUI  |Lower |    Space    |Raise |   é  |   à  |   è  |AltGr |
  * `-----------------------------------------------------------------------------------'
  */
 [_AZERTY] = LAYOUT_planck_grid(
-    KC_DELETE, FR_A,    FR_Z,    FR_E,    FR_R,    FR_T,    FR_Y,    FR_U,    FR_I,    FR_O,    FR_P,    KC_BSPC,
-    KC_TAB,    FR_Q,    FR_S,    FR_D,    FR_F,    FR_G,    FR_H,    FR_J,    FR_K,    FR_L,    FR_M,    KC_QUOT,
-    KC_LSFT,   FR_LABK, FR_W,    FR_X,    FR_C,    FR_V,    FR_B,    FR_N,    FR_COMM, FR_SCLN, FR_COLN, KC_ENT ,
-    KC_ESCAPE, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_DELETE, FR_A,    FR_Z,    FR_E,    FR_R,         FR_T,    FR_Y,    FR_U,         FR_I,    FR_O,    FR_P,    KC_BSPC,
+    KC_TAB,    FR_Q,    FR_S,    FR_D,    FR_F,         FR_G,    FR_H,    FR_J,         FR_K,    FR_L,    FR_M,    FR_QUOT,
+    KC_LSFT,   FR_LABK, FR_W,    FR_X,    FR_C,         FR_V,    FR_B,    FR_N,         FR_COMM, FR_SCLN, FR_COLN, KC_ENT ,
+    KC_ESCAPE, KC_LCTL, KC_LALT, KC_LGUI, MO(_LOWER),   KC_SPC,  KC_SPC,  MO(_RAISE),   FR_EACU, FR_AGRV, FR_EGRV, KC_ALGR
 ),
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   &  |   <  |   >  |   [  |   ]  |   `  |   @  |   +  |   -  |   /  |   *  |   =  |
+ * |      |VerrNm| MouUp|  F12 |   F1 |   F2 |   F3 |RgtClk|  7   |  8   |  9   |  /   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   |  |   ^  |   \  |   (  |   )  |   $  |LftArr|DwnArr| UpArr|RhtArr|      |      |
+ * |      |MouLft|MouDwn|MouRgt|   F4 |   F5 |   F6 |MidClk|  4   |  5   |  6   |  *   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shft |   «  |   »  |   {  |   }  |   ~  |   "  |      |      |      |      |      |
+ * |      | Mute |VolUp |VolDwn|   F7 |   F8 |   F9 |LftClk|  1   |  2   |  3   |  -   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      _      |      |      |      |      |      |
+ * |      |      |      |      |      |  F10 |  F11 |      |  0   |  .   | Entr |  +   |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    BP_AMPR , BP_LABK, BP_RABK, BP_LBRC, BP_RBRC, RALT(BP_EGRV), BP_AT  ,BP_PLUS, BP_MINS, BP_SLSH, BP_ASTR, BP_EQL ,
-    BP_PIPE , BP_CIRC, BP_BSLS, BP_LPRN, BP_RPRN,      BP_DLR  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT,_______,  _______,
-    KC_LSFT , BP_LDAQ, BP_RDAQ, BP_LCBR, BP_RCBR,      BP_TILD , BP_DQUO, _______, _______, _______, _______, _______,
-    _______ , _______, _______, _______, _______,      BP_UNDS , _______, _______, _______, _______, _______, _______
+    _______, KC_NUM_LOCK, KC_MS_UP,        KC_F12,            KC_F1,   KC_F2,  KC_F3,  KC_MS_BTN2, KC_KP_7, KC_KP_8,   KC_KP_9,     KC_KP_SLASH,
+    _______, KC_MS_LEFT,  KC_MS_DOWN,      KC_MS_RIGHT,       KC_F4,   KC_F5,  KC_F6,  KC_MS_BTN3, KC_KP_4, KC_KP_5,   KC_KP_6,     KC_KP_ASTERISK,
+    _______, KC_KB_MUTE,  KC_KB_VOLUME_UP, KC_KB_VOLUME_DOWN, KC_F7,   KC_F8,  KC_F9,  KC_MS_BTN1, KC_KP_1, KC_KP_2,   KC_KP_3,     KC_KP_MINUS,
+    _______, _______,     _______,         _______,           _______, KC_F10, KC_F11, _______,    KC_KP_0, KC_KP_DOT, KC_KP_ENTER, KC_KP_PLUS
 ),
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |  €   |  &   |  %   |  ^   |  [   |  ]   |   -  |   _  |   #  |   `  |   @  |      | V
+ * |  €   |  &   |  %   |  ^   |  [   |  ]   |   -  |   _  |   #  |   `  |   @  |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |CapsLo|AutoKp|      | Yank |   0  |   $  |   #  |   4  |   5  |   6  |   *  |  =   |
+ * |      |  ç   |      |      |  (  |   )   | Left |  Up  | Down | Right|      |  |   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   ^  |  à   |      |      |      |  â   |   %  |   1  |   2  |   3  |   -  | Enter|
+ * |      | °/)  | £/$  | µ *  |  {   |  }   |  §/! |  Deb |  End | Pgup | PgDwn|      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |   ù  |  é   |  è   |  ç   |      |             |   ,  |   0  |   .  |   +  |      |
+ * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    ALGR(FR_AGRV), FR_AMPR, FR_UGRV , FR_CIRC, ALGR(FR_LPRN), ALGR(FR_RPRN), FR_MINS, FR_UNDS, ALGR(FR_DQUO), ALGR(FR_EGRV), ALGR(FR_AGRV), _______,
-    KC_CAPS, C(A(BP_A)),    _______ , TD(TD_YANK) , BP_0   , BP_DLR , BP_HASH,            BP_4    , BP_5   , BP_6   , BP_ASTR, BP_EQL ,
-    FR_CIRC,    FR_AGRV,    _______ ,     _______ , _______, _______, BP_PERC,            BP_1    , BP_2   , BP_3   , BP_MINS, KC_ENT ,
-    FR_UGRV,    FR_EACU,   FR_EGRV  ,    FR_CCED ,  _______, _______, _______, LT(_RAISE, BP_COMM), BP_0   , BP_DOT , BP_PLUS, XXXXXXX
+    ALGR(FR_AGRV), FR_AMPR, FR_UGRV, FR_CIRC, ALGR(FR_LPRN), ALGR(FR_RPRN), FR_MINS, FR_UNDS, ALGR(FR_DQUO), ALGR(FR_EGRV), ALGR(FR_AGRV), ALGR(FR_UNDS),
+    _______,       FR_CCED, _______, _______, FR_LPRN,       FR_RPRN,       KC_LEFT, KC_UP,   KC_DOWN,       KC_RIGHT,      _______,       ALGR(FR_MINS),
+    _______,       FR_RPRN, FR_DLR,  FR_ASTR, ALGR(FR_QUOT), ALGR(FR_EQL),  FR_EXLM, KC_HOME, KC_END,        KC_PAGE_UP,    KC_PAGE_DOWN,  _______,
+    _______,       _______, _______, _______, _______,       _______,       _______, _______,  _______,       _______,       _______,       _______
 ),
 /* Adjust (Lower + Raise)
  *                      v------------------------RGB CONTROL--------------------v
@@ -108,13 +101,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_planck_grid(
     _______, QK_BOOT, DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
-    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  AZERTY,   BEPO,    _______, KC_PAUSE,
+    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______,  AZERTY,  _______, _______, KC_PAUSE,
     _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, KC_PRINT_SCREEN,
     BACKLIT, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
 )
 
 };
-
 
 layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
@@ -122,22 +114,9 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-        print("mode just switched to qwerty and this is a huge string\n");
-        set_single_persistent_default_layer(_QWERTY);
-      }
-      return false;
-      break;
     case AZERTY:
       if (record->event.pressed) {
         set_single_persistent_default_layer(_AZERTY);
-      }
-      return false;
-      break;
-    case BEPO:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_BEPO);
       }
       return false;
       break;
@@ -247,8 +226,4 @@ bool music_mask_user(uint16_t keycode) {
   }
 }
 
-qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_YANK]  = ACTION_TAP_DANCE_DOUBLE(C(KC_INS), C(BP_C)),
-    [TD_PASTE] = ACTION_TAP_DANCE_DOUBLE(S(KC_INS), C(BP_V))
-};
 
