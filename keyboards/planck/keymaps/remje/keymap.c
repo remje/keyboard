60e79a91c32e91ca63ff4d3bbdd8c40f8738ed60
remje <arenwpro@gmail.com>
2023-02-23 09:58:46 +0100

    numbers not from numpad

--- keyboards/planck/keymaps/remje/keymap.c
+++ keyboards/planck/keymaps/remje/keymap.c
@@ -67,10 +67,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    _______, KC_NUM_LOCK, KC_MS_UP,        KC_F12,            KC_F1,   KC_F2,  KC_F3,  KC_MS_BTN2, KC_KP_7, KC_KP_8,   KC_KP_9,     KC_KP_SLASH,
-    _______, KC_MS_LEFT,  KC_MS_DOWN,      KC_MS_RIGHT,       KC_F4,   KC_F5,  KC_F6,  KC_MS_BTN3, KC_KP_4, KC_KP_5,   KC_KP_6,     KC_KP_ASTERISK,
-    _______, KC_KB_MUTE,  KC_KB_VOLUME_UP, KC_KB_VOLUME_DOWN, KC_F7,   KC_F8,  KC_F9,  KC_MS_BTN1, KC_KP_1, KC_KP_2,   KC_KP_3,     KC_KP_MINUS,
-    _______, _______,     _______,         _______,           _______, KC_F10, KC_F11, _______,    KC_KP_0, KC_KP_DOT, KC_KP_ENTER, KC_KP_PLUS
+    _______, KC_NUM_LOCK, KC_MS_UP,        KC_F12,            KC_F1,   KC_F2,  KC_F3,  KC_MS_BTN2, LSFT(FR_EGRV), LSFT(FR_UNDS), LSFT(FR_CCED),     KC_KP_SLASH,
+    _______, KC_MS_LEFT,  KC_MS_DOWN,      KC_MS_RIGHT,       KC_F4,   KC_F5,  KC_F6,  KC_MS_BTN3, LSFT(FR_QUOT), LSFT(FR_LPRN), LSFT(FR_MINS),     KC_KP_ASTERISK,
+    _______, KC_KB_MUTE,  KC_KB_VOLUME_UP, KC_KB_VOLUME_DOWN, KC_F7,   KC_F8,  KC_F9,  KC_MS_BTN1, LSFT(FR_AMPR), LSFT(FR_EACU), LSFT(FR_DQUO),     KC_KP_MINUS,
+    _______, _______,     _______,         _______,           _______, KC_F10, KC_F11, _______,    LSFT(FR_AGRV), KC_KP_DOT,     KC_KP_ENTER,       KC_KP_PLUS
 ),
 /* Lower
  * ,-----------------------------------------------------------------------------------.
